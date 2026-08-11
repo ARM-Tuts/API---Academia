8d070dc1b63f88fa12eccb2aa6b0d0618ff70c72
Arthur Rodrigues <arthur.rms2018@gmail.com>
2026-08-11 19:15:26 -0300

    Adicionando biblioteca CORS para o Front-end consumir a API

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const authRouter = require('./routes/auth.js');
 const alunosRouter = require('./routes/alunos.js');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/auth', authRouter);
 app.use('/alunos', alunosRouter);
